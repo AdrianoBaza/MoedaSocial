30e7dd50f8f07575d2967c17c87a725ba6b02410
Deivyson José da Silva <139878479+Deivyson1401@users.noreply.github.com>
2024-01-11 14:58:22 +0000

    inserindo config banco de dados

--- conect_bd/sequelize.js
+++ conect_bd/sequelize.js
@@ -1,21 +1,121 @@
 const Sequelize = require('sequelize')
+const { SELECT } = require('sequelize/types/query-types')
 
 const conect_bd = new Sequelize('moeda_social','root','exemplo123',{
     host: "localhost",
     dialect: "mysql"
 })
 
-let perfil_estudante = conect_bd.define('perfil',{
-    nome: Sequelize.STRING,
-    matricula: Sequelize.STRING,
-    senha: Sequelize.STRING,
-    saldo: Sequelize.FLOAT
+const perfil_estudante = conect_bd.define('perfil',{
+    id: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        primaryKey: true
+    },
+    nome: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        primaryKey: false
+    },
+    matricula: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        primaryKey: true
+    },
+    senha: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        primaryKey: false
+    },
+    saldo: {
+        type: Sequelize.FLOAT,
+        allowNull: false,
+        primaryKey: false
+    }
 })
 
+const Extrato_entrada = conect_bd.define('entradas', {
+    nome_remetente: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        primaryKey: false
+    },
+    id_remetente: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        primaryKey: true
+    },
+    valor: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        primaryKey: false
+    },
+    hora: {
+        type: Sequelize.TIME,
+        allowNull: false,
+        primaryKey: false
+    },
+    data: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        primaryKey: false
+    }
+})
 
-function new_perfil_estudante(nome, matricula, senha) {
+const Extrato_saída = conect_bd.define('saídas', {
+    nome_destinatario: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        primaryKey: false
+    },
+    id_destinatario: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        primaryKey: true
+    },
+    valor: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        primaryKey: false
+    },
+    hora: {
+        type: Sequelize.TIME,
+        allowNull: false,
+        primaryKey: false
+    },
+    data: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        primaryKey: false
+    }
+})  
+
+function new_ransfer_entrada(id, valor, data, hora) {
+    conect_bd.sync().then(()=>{
+        Extrato_entrada.create({
+            id: id,
+            valor:valor,
+            data: data,
+            hora: hora
+        })
+    })
+}
+
+function new_ransfer_entrada(id, valor, data, hora) {
+    conect_bd.sync().then(()=>{
+        Extrato_entrada.create({
+            id: id,
+            valor:valor,
+            data: data,
+            hora: hora
+        })
+    })
+}
+
+function new_perfil_estudante(id,nome, matricula, senha) {
     conect_bd.sync().then(()=>{
         perfil_estudante.create({
+            id: id,
             nome: nome,
             matricula: matricula,
             senha:senha,
@@ -23,7 +123,6 @@ function new_perfil_estudante(nome, matricula, senha) {
         })
     })
 }
-
 
 conect_bd.authenticate().then(()=>{
     console.log('conectado ao banco de dados');
